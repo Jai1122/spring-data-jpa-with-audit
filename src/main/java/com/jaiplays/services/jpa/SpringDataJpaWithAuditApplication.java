9628d9c3ce80e905562e92b875c385072ddae4f5
Jai <jayapal1122@gmail.com>
2021-08-04 15:07:31 +0530

    Initial Commit

---
+++ src/main/java/com/jaiplays/services/jpa/SpringDataJpaWithAuditApplication.java
@@ -0,0 +1,13 @@
+package com.jaiplays.services.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWithAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaWithAuditApplication.class, args);
+	}
+
+}
